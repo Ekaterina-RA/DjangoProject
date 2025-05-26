41d048658c3b10737f88d027a775d7d3becd9b7c
Ekaterina <ekaterina.kuz@gmail.com>
2025-05-26 19:54:11 +0300

    some corrections done to catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,7 @@ from catalog.views import product_detail, one_product
 app_name = CatalogConfig.name
 urlpatterns = [
     path("", views.home, name="home"),
-    #path("contacts/", views.contacts, name="contacts"),
+    path("contacts/", views.contacts, name="contacts"),
     path('product_detail/', product_detail, name='product_detail'),
     path('one_product/<int:pk>/', one_product, name='one_product'),
 ]
