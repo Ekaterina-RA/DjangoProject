5a03b570fb8436e6aaf004e91c8288acfea5eace
Ekaterina <ekaterina.kuz@gmail.com>
2025-06-08 12:48:08 +0300

    models updated

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView, TemplateView
-from catalog.models import Product
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Product
+from .forms import ProductForm
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -9,14 +11,33 @@ class HomeView(TemplateView):
 class ContactsView(TemplateView):
     template_name = "contacts.html"
 
-
-class ProductListView(ListView):
-    model = Product
-    template_name = 'product_list.html'
-
-
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'one_product.html'
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = reverse_lazy('product_list')
+
+
+class ProductListView(ListView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = reverse_lazy('product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
 
